6e35bebb9b5865f6bde1470403ec3c7d085f613d
Isa S <smiba@users.noreply.github.com>
2024-03-28 23:54:20 +0100

    issue route no longer broke (#8)
    Co-authored-by: Stella <hello@koding.dev>

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -32,11 +32,9 @@ export const addIndexRoutes = (app: Hono<{ Bindings: Bindings }>) => {
       c,
       () => ({
         title: "Report an issue",
-        description: `To report an issue, go to ${getBaseURL(
-          c,
-        )}/issue. (Or click the blue link)`,
+        description: "To report an issue, go to https://github.com/Britmoji/tiktxk/issues/new. (Or click the blue link)",
       }),
-      () => c.redirect(`${getBaseURL(c)}/issue`),
+      () => c.redirect("https://github.com/Britmoji/tiktxk/issues/new"),
     ),
   );
 };
